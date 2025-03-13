50ed6ef6f09940c7c09231967dde24a6ad51ac67
HazelAmi <dalafeghaquakumey@byupathway.edu>
2025-03-13 22:39:31 +0000

    DOM commit

---
+++ week02/scripts/bom.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('#Favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('________');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+deleteButton.textContent = '❌';
+
+li.append(deleteButton);
+li.append(li);
