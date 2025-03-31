70dd02412da87f14d22dd35831e85606f54d5aa8
HazelAmi <dalafeghaquakumey@byupathway.edu>
2025-03-31 22:35:49 +0000

    Edit to week 4 album

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -137,7 +137,7 @@ function createTempleCard(filteredTemples) {
     document.querySelector(".res-grid").innerHTML = ""; // Clear existing content
     filteredTemples.forEach(temple => {
         let card = document.createElement("section");
-        let name = document.createElement("h3");
+        let name = document.createElement("h2");
         let location = document.createElement("p");
         let dedication = document.createElement("p");
         let area = document.createElement("p");
